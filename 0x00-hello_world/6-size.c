2ad01c3341fb11d24db3a9b8e73da6cb7709cf87
CarltonNcube <carltonncube@outlook.com>
2023-02-18 04:56:21 -0800

    5-printf.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -Program that prints the size of various types on the computer
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+       char c;
+       int i;
+
+       	printf( 
+     	return (0);
+}
