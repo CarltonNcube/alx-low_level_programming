9aeffc463058707b6b7231e5ecb5efb1f6d3cadd
CarltonNcube <carltonncube@outlook.com>
2023-02-18 14:06:17 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
 printf("size of a char: %lu byte(s)\n",(unasigned long)sizeof(a)) 
 printf("size of a int: %lu byte(s)\n",(unasigned long)sizeof(b));
-printf("size of a long int: %lu byte(s)\n",(unasigned long)sizeof(c));
-printf("size of a long long int: %lu byte(s)\n",(unasigned long)sizeof(d));
-printf("size of a float: %lu byte(s)\n",(unasigned long)sizeof(f));
+printf("size of a long int: %lu bytes(s)\n",(unasigned long)sizeof(c));
+printf("size of a long long int: %lu bytes(s)\n",(unasigned long)sizeof(d));
+printf("size of a float: %lu bytes(s)\n",(unasigned long)sizeof(f));
 return (0);
 }
