da0f04044273d23fca62d6862936295c37c8a323
CarltonNcube <carltonncube@outlook.com>
2023-02-18 14:41:39 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - A program that prints a line, to the standard error
  *
@@ -8,7 +7,6 @@
  */
 int main(void)
 {
-
 fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
