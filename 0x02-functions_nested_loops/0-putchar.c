163485b7d38c8be0c03551d7248ef0484cea39fa
CarltonNcube <carltonncube@outlook.com>
2023-02-21 12:56:00 -0800

    putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include<stdio.h>
+#include "main.h"
 
 /**
- * void display();
- * int main()
-{ int a;
-  display();
-  return (0);
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+   _putchar("_");
+   _putchar("p");
+   _putchar("u");
+   _putchar("t");
+   _putchar("c");
+   _putchar("h");
+   _putchar("a");
+   _putchar("r");
+    return (0);
 }
- void display(){
-      printf("_putchar");}
