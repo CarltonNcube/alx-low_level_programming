1d512205b235020efe44aea3e630a6c70addbff9
CarltonNcube <carltonncube@outlook.com>
2023-02-21 09:25:10 -0800

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - A C program that prints exactly "Programming is like building a multilingual puzzle
+ * main - A C program that prints using puts function
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 { 
-	puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
