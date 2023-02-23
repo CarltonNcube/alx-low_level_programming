fab5ab6f7cf83ef3d44b6323dbc7f9e8228b35eb
CarltonNcube <carltonncube@outlook.com>
2023-02-23 10:34:10 -0800

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet- A program which prints an alphabet
+ * print_alphabet- Make an alphabet
  *
- * Return: void.
+ * Return: void
  */
 void print_alphabet(void)
 {0
     char c;
-    for (c = 'a'; c <= 'z'; c++)
+    for ( c = 'a'; c <= 'z'; c++)
     {
     _putchar(c);
     }
