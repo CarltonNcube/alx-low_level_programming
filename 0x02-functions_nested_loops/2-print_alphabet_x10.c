ae2d4fb06c5db782e1dbd4a8c2b54ee6a6cb8ef0
CarltonNcube <carltonncube@outlook.com>
2023-02-23 11:04:31 -0800

    Print alphabet x 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,17 @@
 #include "main.h"
-
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_alphabet - make an alphabet x 10
+ * Return: void
  */
 void print_alphabet_x10(void);
 {
-    print_alphabet_x10();
-    return (0);
+char c;
+int i = 0;
+while (i <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+_putchar('\n');
+}
 }
