be79c15d842dd846483cbcd4e7d813b03d753fa2
CarltonNcube <carltonncube@outlook.com>
2023-02-23 12:05:39 -0800

    Is alpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  *@c: the charecter to be checked
  * Return: 1 for alphabetic character or 0 for anything else
  */
-int _isalpha(int c
+int _isalpha(int c)
 {
 if ((c >= 65 && c <= 90 || (c <= 122));
 {
