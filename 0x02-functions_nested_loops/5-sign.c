31d806ae096d7fe4a7312e0570f7975258189c7f
CarltonNcube <carltonncube@outlook.com>
2023-02-23 13:16:59 -0800

    Sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - print the sign of a number
- *@n: the number to be chacked
+ * @n: the number to be chacked
  * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
 int print_sign(int n)
