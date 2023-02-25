740966f31991b31f018af7c5500e31c687b77ab9
CarltonNcube <carltonncube@outlook.com>
2023-02-25 10:08:39 -0800

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,11 +6,13 @@
  * @n: the value of the times table to be printed.
  */
 void print_times_table(int n)
+
 {
 	int num, mult, prod;
+
 	if (n >= 0 && n <= 15)
 	{
-	for (num =0; num <= n; mult++)
+	for (num = 0; num <= n; mult++)
 	{
 	_putchar('0');
 	for (mult = 1; mult <= n; mult++)
@@ -24,14 +26,14 @@ void print_times_table(int n)
 	_putchar(' ');
 	if (prod >= 100)
 	{
-	_putchar((prod /100) + '0');
-	_putchar(((prod /10)) % 10 + '0');
+	_putchar((prod / 100) + '0');
+	_putchar(((prod / 10)) % 10 + '0');
 	}
 	else if (prod <= 99 && prod >= 10)
 	{
-	_putchar((prod /10) + '0');
+	_putchar((prod / 10) + '0');
 	}
-	_putchar((prod /10) % + '0');
+	_putchar((prod / 10) % + '0');
 	}
 	_putchar('\n');
 	}
