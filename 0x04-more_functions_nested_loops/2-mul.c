ab4d1778260d97cbc45255485422c8a42168436b
CarltonNcube <carltonncube@outlook.com>
2023-02-26 11:21:35 -0800

    Multiply function

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * mult -Multiplie two integers
- * @a: The first number to be multiplied 
+ * mul -Multiplie two integers
+ * @a: The first number to be multiplied
  * @b: The second number to multiply with the first number
  * Return: The result of multiplication between a and b
  */
