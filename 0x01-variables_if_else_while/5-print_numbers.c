9b1bbf8a08847919f5864228c39b56190942be08
CarltonNcube <carltonncube@outlook.com>
2023-02-25 23:50:54 -0800

    Print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,10 +5,13 @@
  * main - Entry point
  * Return: Always 0 (success)
  */
+
 int main(void)
+
 {
+	
 	int i;
-	for (i = 0; i <10; i++)
+	for (i = 0; i < 10; i++)
 	printf("%d", i);
 	printf("\n");
 
