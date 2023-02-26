d54a8e7e50b2b8034f0f74b9704a3340afd88226
CarltonNcube <carltonncube@outlook.com>
2023-02-26 00:01:52 -0800

    Print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (success)
@@ -9,7 +10,7 @@
 int main(void)
 
 {
-	
+
 	int i;
 
 	for (i = 0; i < 10; i++)
