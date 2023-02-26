844ff0400be99ad448909a24e3d63b97e0457515
CarltonNcube <carltonncube@outlook.com>
2023-02-26 11:29:47 -0800

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers -Print the numbers from 0 to 9
+ * Return: Numbers from 0 to 9
+ */
+void print_numbers(void)
+
+{
+	int x;
+	for (x = 0; X <= 9; x++)
+	{
+	putchar(x + '0');
+	}
+	putchar('\n');
+}
