d42ddcc17f9a919270ea454d53abbb7b9b6c4173
CarltonNcube <carltonncube@outlook.com>
2023-02-26 06:46:30 -0800

    Alphabet in lower & uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Prints the alphabet in lowercase, and then in uppercase followed by a newline
+ * main - Prints the alphabet in lowercase, and in uppercase
  * Return: Always 0 (sucess)
  */
+
 int main(void)
 {
-char low;
-for (low = 'a'; low <= 'z'; low++)
-putchar(low);
-for (low = 'A'; low <= 'Z'; low++)
-putchar(low);
-putchar('\n');
-return (0);
+
+	char low;
+	
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+	putchar(low);
+	putchar('\n');
+	
+	return (0);
 }
