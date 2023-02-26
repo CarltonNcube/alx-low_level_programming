454dc8f8579ad725301e5ab4dbcda8fbcda34014
CarltonNcube <carltonncube@outlook.com>
2023-02-26 10:26:58 -0800

    I supper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if a letter is upper
+ * @a: The number to be checked 
+ * Return: 1 for upper case or 0 for any else
+ */
+int _isupper(int a)
+{
+	if (a >= 65 && <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
+
