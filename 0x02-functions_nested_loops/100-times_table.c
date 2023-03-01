e6ca0a25cd49bc3bc3a3faad05ec3b31966a200d
CarltonNcube <carltonncube@outlook.com>
2023-03-01 07:52:55 -0800

    function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints time table of the input,
+ * print_times_table - Prints times table of the input,
  * starting with 0.
  * @n: The value of the times table to be printed.
  */
