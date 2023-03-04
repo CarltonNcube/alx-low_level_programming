e66687bb683890fe4faf0ddad26e949a1da8482d
CarltonNcube <carltonncube@outlook.com>
2023-03-04 11:51:03 -0800

    Fibonacci sequence is generated by adding the previous two terms

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,21 +3,21 @@
 /**
  * main - Prints the sum of even fibonacci numbers
  * less than 4000000
- * Return: Nothing 
+ * Return: Nothing!
  */
 
 int main(void)
 
 {
 	int i = 0;
-	long j = 1 k = 2, sum = k;
+	long j = 1, k = 2, sum = k;
 
 	while (k + j < 4000000)
 	{
 	k += j;
 	if (k % 2 == 0)
 	sum += k;
-	j = k -j;
+	j = k - j;
 	++i;
 	}
 	printf("%ld\n", sum);
