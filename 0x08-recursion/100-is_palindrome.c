d57f47e495aea8d7166c69b6e8c7288e0494d3e1
CarltonNcube <carltonncube@outlook.com>
2023-03-09 18:52:25 -0800

    function that returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -18,7 +18,7 @@ int _strlen_recursion(char *s)
  * @s: string
  * @n1: smallest iterator.
  * @n2: biggest iterator
- * Return (0)
+ * Return: (0)
  */
 int comparator(char *s, int n1, int n2)
 {
@@ -26,7 +26,7 @@ int comparator(char *s, int n1, int n2)
 	{
 		if (n1 == n2 || n1 == n2 + 1)
 		return (1);
-		return (0 + comparator(s, n1 + 1, n2 -1));
+		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
 }
@@ -39,5 +39,5 @@ int is_palindrome(char *s)
 {
 	if (*s == '\0')
 	return (1);
-	return (comparator(s, 0, _strlen_recursion(s) -1));
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
