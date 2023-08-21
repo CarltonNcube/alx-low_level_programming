4559909102746a0c422c4d6a4ccd858cb98230f7
CarltonNcube <carltonncube@outlook.com>
2023-08-21 20:38:08 +0000

    A library is not a luxury but one of the necessities of life

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Compile and create the object files
-gcc -c -fPIC *.c
+# Get a list of all .c files in the current directory
+c_files=$(ls *.c)
 
-# Create the dynamic library
+# Compile each .c file into an object file
+for c_file in $c_files; do
+    gcc -c -fPIC $c_file -o ${c_file%.c}.o
+done
+
+# Create the dynamic library liball.so from the object files
 gcc -shared -o liball.so *.o
 
-# Clean up object files
-rm -f *.o
+# Clean up the object files
+rm *.o
