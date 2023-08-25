cb0a2357470b55fc8c3de725120178ec7da0c707
CarltonNcube <carltonncube@outlook.com>
2023-08-25 16:56:22 -0700

    dynamic library that contains C functions that can be called from Python

--- 0x18-dynamic_libraries/100-tests.py
+++ 0x18-dynamic_libraries/100-tests.py
@@ -1,13 +1,34 @@
 import random
 import ctypes
 
-cops = ctypes.CDLL('./100-operations.so')
+# Load the shared library
+cops = ctypes.CDLL('./100-operations.so')  # Make sure to use the correct path
 
-a = random.randint(-111, 111)
-b = random.randint(-111, 111)
+def test_operations():
+    a = random.randint(-111, 111)
+    b = random.randint(-111, 111)
 
-print("{} + {} = {}".format(a, b, cops.add(a, b)))
-print("{} - {} = {}".format(a, b, cops.sub(a, b)))
-print("{} x {} = {}".format(a, b, cops.mul(a, b)))
-print("{} / {} = {}".format(a, b, cops.my_div(a, b)))
-print("{} % {} = {}".format(a, b, cops.mod(a, b)))
+    print(f"Random numbers: {a}, {b}")
+
+    result_add = cops.add(a, b)
+    print(f"{a} + {b} = {result_add}")
+
+    result_sub = cops.sub(a, b)
+    print(f"{a} - {b} = {result_sub}")
+
+    result_mul = cops.mul(a, b)
+    print(f"{a} * {b} = {result_mul}")
+
+    if b != 0:  # Avoid division by zero
+        result_div = cops.div(a, b)
+        print(f"{a} / {b} = {result_div}")
+        result_mod = cops.mod(a, b)
+        print(f"{a} % {b} = {result_mod}")
+    else:
+        print("Division by zero not possible")
+
+if __name__ == "__main__":
+    for _ in range(5):  # You can adjust the number of test cases
+        print("===")
+        test_operations()
+
