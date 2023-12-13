db50a4474aabfcec4827fe2aa9f40ce5e582be03
CarltonNcube <carltonncube@outlook.com>
2023-12-13 10:31:58 -0800

    a function that searches for a value in an array of integers using the Linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,16 +10,20 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    if (array == NULL || size == 0)
-        return -1;
+	size_t i = 0;
 
-    for (size_t i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	if (!array)
+		return (-1);
 
-        if (array[i] == value)
-            return i;
-    }
+	while (i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-    return -1;
+		if (array[i] == value)
+			return (i);
+
+		i++;
+	}
+
+	return (-1);
 }
